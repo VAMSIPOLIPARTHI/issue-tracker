f9b96021e5ef0673546260deacc1628af8c7b4e5
Poliparthi Vamsi <152620111+VAMSIPOLIPARTHI@users.noreply.github.com>
2025-12-30 09:39:52 +0530

    Delete issue-tracker directory

--- issue-tracker/js/config.js
+++
@@ -1,8 +0,0 @@
-export const firebaseConfig = {
-    apiKey: "AIzaSyCPqCSrxyoH2uEf7XavHDlrKcNSZg08ijk",
-    authDomain: "issue-tracker-ada42.firebaseapp.com",
-    projectId: "issue-tracker-ada42",
-    storageBucket: "issue-tracker-ada42.firebasestorage.app",
-    messagingSenderId: "1002860261626",
-    appId: "1:1002860261626:web:80c566a6a4b2283aee9943"
-};
